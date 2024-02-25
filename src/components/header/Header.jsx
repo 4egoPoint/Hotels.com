f85a4e8e4036341a1e93e2684f5f75b0764c6906
Pavlo Sokolovskyi <mainfhr@gmail.com>
2024-02-25 23:31:03 +0100

    header2

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,45 @@
 
 
 import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
+import { IoMdDownload } from "react-icons/io";
 import "./header.scss"
 import { useState } from "react";
+import Nav from "./Nav";
 
-const routesArr = []
+const routesArr = [
+   {
+      id: 1,
+      navType: "link",
+      name: "List your property",
+      link: "",
+      route: "",
+   },
+   {
+      id: 2,
+      navType: "route",
+      name: "Support",
+      link: "",
+      route: "",
+   },
+   {
+      id: 3,
+      navType: "route",
+      name: "Tips",
+      link: "",
+      route: "",
+   },
+   {
+      id: 4,
+      navType: "element",
+      name: "Sign in",
+      link: "",
+      route: "",
+   }, 
+]
 
 const Header = () => {
    const [isDropdownOpen, setIsDropdownOpen] = useState(false)
+   const [isSignInOpen, setIsSignInOpen] = useState(true)
    return (
       <div className="header">
          <div className="header__wrapper">
@@ -15,15 +47,26 @@ const Header = () => {
                <h1 className="header__logo">Hotels<span>.com</span></h1>
                <button onClick={() => setIsDropdownOpen(!isDropdownOpen)} className="header__dropdown">Shop travel {isDropdownOpen ? <MdKeyboardArrowUp /> : <MdKeyboardArrowDown />}</button>
                {
-                  !isDropdownOpen ? <></> : <ul className="header__droped-menu">
+                  isDropdownOpen ? <ul className="header__droped-menu">
                      <li>Groups & meetings</li>
                      <li>Hotels.com Rewards</li>
-                  </ul>
+                  </ul> : null
                }
             </div>
+            {isDropdownOpen ? <button onClick={() => setIsDropdownOpen(!isDropdownOpen)} className="katalizator"></button> : null}
             <div className="header__row">
-               <div className="header__get-app"></div>
-               <div className="header__route"></div>
+               <button className="header__get-app"><IoMdDownload  /><span>Get the app</span></button>
+               <nav className="header__nav">
+                  {
+                     routesArr.map((item)=> <Nav key={item.id} 
+                     navType={item.navType} 
+                     name={item.name} 
+                     link={item.link} 
+                     route={item.route}
+                     isSignInOpen={isSignInOpen}
+                     setIsSignInOpen={setIsSignInOpen}  />)
+                  }
+               </nav>
             </div>
          </div>
       </div>
---
+++ src/components/header/Nav.jsx
@@ -0,0 +1,34 @@
+
+
+import { Link } from "react-router-dom"
+import "./header.scss"
+
+const Nav = ({ navType, name, link, route, isSignInOpen, setIsSignInOpen }) => {
+   if (navType === "link") {
+      return (
+         <a href={link} className='link'>{name}</a>
+      )
+   }
+   if (navType === "route") {
+      return (
+         <Link to={route} className="route">{name}</Link>
+      )
+   }
+   if (navType === "element") {
+      return (
+         <>
+            <div className="element">
+               <button onClick={() => setIsSignInOpen(!isSignInOpen)} className="element__name">{name}</button>
+               {isSignInOpen ? <div className="sign">
+                  <h3>Members save 10% or more on over 100,000 hotels worldwide when you're signed in</h3>
+                  <button className="element__btn-blue">Sign in</button>
+                  <button className="element__btn">Sign up, it's free</button>
+               </div> : null}
+            </div>
+            {isSignInOpen ? <button onClick={() => setIsSignInOpen(false)} className="katalizator"></button> : null}
+         </>
+      )
+   }
+}
+
+export default Nav
